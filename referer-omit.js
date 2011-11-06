7ad53ab685204df0813f4a04c31a1eb18c76b20d
Gea-Suan Lin <darkkiller@gmail.com>
2011-11-06 22:38:41 +0800

    Use url to replace req.url

--- referer-omit.js
+++ referer-omit.js
@@ -14,10 +14,11 @@
 
     var filter = function(req){
         var hdrs = req.requestHeaders;
+        var url = req.url;
 
         for (var i = 0; i < hdrs.length; i++) {
             if ('Referer' == hdrs[i].name) {
-                var r = req.url.match(/^(\w+:\/\/[^\/]+(:\d+)?\/?)/);
+                var r = url.match(/^(\w+:\/\/[^\/]+(:\d+)?\/?)/);
                 if (r) {
                     hdrs[i].value = r[1];
                 }
