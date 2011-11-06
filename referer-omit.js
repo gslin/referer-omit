4b586ab1842f7eac90b615c65b77c7de03d2893b
Gea-Suan Lin <darkkiller@gmail.com>
2011-11-06 22:48:46 +0800

    Support for _hostname and _remove

--- referer-omit.js
+++ referer-omit.js
@@ -16,15 +16,29 @@
         var hdrs = req.requestHeaders;
         var url = req.url;
 
-        var action = getAction(url);
-
-        for (var i = 0; i < hdrs.length; i++) {
-            if ('Referer' == hdrs[i].name) {
-                var r = url.match(/^(\w+:\/\/[^\/]+(:\d+)?\/?)/);
-                if (r) {
-                    hdrs[i].value = r[1];
+        switch (getAction(url)) {
+        case '_hostname':
+            for (var i = 0; i < hdrs.length; i++) {
+                if ('Referer' == hdrs[i].name) {
+                    hdrs.splice(i, 1);
                 }
             }
+
+            var ref = url.match(/^(\w+:\/\/[^\/]+(:\d+)?\/?)/);
+            if (ref) {
+                hdrs.push({name: 'Referer', value: ref[1]});
+            }
+
+            break;
+        case '_remove':
+            for (var i = 0; i < hdrs.length; i++) {
+                if ('Referer' == hdrs[i].name) {
+                    hdrs.splice(i, 1);
+                }
+            }
+            break;
+        default:
+            break;
         }
 
         return {requestHeaders: hdrs};
