30c77e818461e4b8e02ef7a295bab4f35ed51d0b
Gea-Suan Lin <darkkiller@gmail.com>
2011-11-06 22:26:41 +0800

    Set all referer to hostname itself.

---
+++ referer-omit.js
@@ -0,0 +1,22 @@
+(function(){
+    var filter = function(req){
+        var hdrs = req.requestHeaders;
+
+        for (var i = 0; i < hdrs.length; i++) {
+            if ('Referer' == hdrs[i].name) {
+                var r = req.url.match(/^(\w+:\/\/[^\/]+(:\d+)?\/?)/);
+                if (r) {
+                    hdrs[i].value = r[1];
+                }
+            }
+        }
+
+        return {requestHeaders: hdrs};
+    };
+
+    chrome.experimental.webRequest.onBeforeSendHeaders.addListener(
+        filter,
+        {},
+        ['blocking', 'requestHeaders']
+    );
+})();
